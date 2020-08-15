f6bd0bd239d200209765ada45478ae7ff8b5e925
Lionel Armanet <larmanet@salesforce.com>
2020-08-15 09:03:23 +0200

    chore: adds prettier / eslint config

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const readline = require('readline');
-const exif = require('jpeg-exif');
-const { Readable, Writable, Transform, pipeline } = require('stream');
-const { Pool } = require('pg')
-const { recursiveScan } = require('./src/fs_util');
-const uuid = require('uuid');
-const path = require('path');
+const readline = require("readline");
+const exif = require("jpeg-exif");
+const { Readable, Writable, Transform, pipeline } = require("stream");
+const { Pool } = require("pg");
+const { recursiveScan } = require("./src/fs_util");
+const uuid = require("uuid");
+const path = require("path");
 
 function spinner(message) {
     var _message = message;
 
-    const spinner = '▁▃▄▅▆▇█▇▆▅▄▃';
+    const spinner = "▁▃▄▅▆▇█▇▆▅▄▃";
     var spinnerPosition = 0;
     const printWithSpinner = (message) => {
         spinnerPosition = (spinnerPosition + 1) % spinner.length;
         readline.cursorTo(process.stdout, 0);
         process.stdout.write(`${spinner.charAt(spinnerPosition)} ${message}`);
-    }
+    };
 
     var interval = setInterval(() => {
         printWithSpinner(_message);
@@ -23,7 +23,7 @@ function spinner(message) {
     printWithSpinner(_message);
 
     return {
-        setMessage: message => {
+        setMessage: (message) => {
             _message = message;
         },
         stop: () => {
@@ -31,10 +31,10 @@ function spinner(message) {
             printWithSpinner(_message);
         },
         cleanup: () => {
-            readline.cursorTo(process.stdout, 0);    
-            process.stdout.write('');
+            readline.cursorTo(process.stdout, 0);
+            process.stdout.write("");
         }
-    }
+    };
 }
 
 async function gatherAllFilenamesFromFS(rootUrl) {
@@ -44,20 +44,28 @@ async function gatherAllFilenamesFromFS(rootUrl) {
         fileNames: []
     };
     return new Promise((resolve, reject) => {
-        recursiveScan(rootUrl, ['.jpg','.png','.jpeg','.gif','.png'])
-            .on('omit', () => {
+        recursiveScan(rootUrl, [".jpg", ".png", ".jpeg", ".gif", ".png"])
+            .on("omit", () => {
                 stats.removed++;
             })
-            .on('data', data => {
+            .on("data", (data) => {
                 stats.filtered++;
                 stats.fileNames.push(String(data));
-                sp.setMessage(`Recursively scanning ${rootUrl}, ${stats.filtered} / ${stats.filtered + stats.removed}`);
+                sp.setMessage(
+                    `Recursively scanning ${rootUrl}, ${stats.filtered} / ${
+                        stats.filtered + stats.removed
+                    }`
+                );
             })
-            .on('finish', () => {
-                sp.setMessage(`Recursively scanning ${rootUrl}, ${stats.filtered} / ${stats.filtered + stats.removed}`);
+            .on("finish", () => {
+                sp.setMessage(
+                    `Recursively scanning ${rootUrl}, ${stats.filtered} / ${
+                        stats.filtered + stats.removed
+                    }`
+                );
                 resolve(stats);
             })
-            .on('error', (error) => {
+            .on("error", (error) => {
                 reject(error);
             });
     });
@@ -80,43 +88,50 @@ class FileNamesStream extends Readable {
     }
 }
 
-const exifExtractor = rootUrl => new Transform({
-    objectMode: true,
-    transform(chunk, encoding, callback) {
-        const self = this;
-        const filePath = String(chunk);
-        exif.parse(filePath, (err, data) => {
-            if (err) {
-                self.emit('failed', filePath);
+const exifExtractor = (rootUrl) =>
+    new Transform({
+        objectMode: true,
+        transform(chunk, encoding, callback) {
+            const self = this;
+            const filePath = String(chunk);
+            exif.parse(filePath, (err, data) => {
+                if (err) {
+                    self.emit("failed", filePath);
+                    return callback();
+                }
+                callback(null, {
+                    filePath: filePath,
+                    meta: {
+                        exif: data,
+                        tags: path
+                            .dirname(filePath.replace(rootUrl, ""))
+                            .split("/")
+                            .filter((x) => x.length > 0)
+                    }
+                });
+            });
+        }
+    });
+
+const omitIndexed = (alreadyIndexed) =>
+    new Transform({
+        transform(chunk, encoding, callback) {
+            const filePath = String(chunk);
+            if (alreadyIndexed.includes(filePath)) {
                 return callback();
             }
-            callback(null, { 
-                filePath: filePath,
-                meta: {
-                    exif: data,
-                    tags: path.dirname(filePath.replace(rootUrl, '')).split('/').filter(x => x.length > 0)
-                }
-            });
-        });
-    }
-});
 
-const omitIndexed = alreadyIndexed => new Transform({
-    transform(chunk, encoding, callback) {
-        const filePath = String(chunk);
-        if(alreadyIndexed.includes(filePath)) {
-            return callback();
+            return callback(null, chunk);
         }
-
-        return callback(null, chunk);
-    }
-});
+    });
 
 async function loadIndexedFilenames() {
     const client = await pool.connect();
     try {
-        const result = await client.query('select path from photo_scanner.photos');
-        return result.rows.map(row => row['path']);
+        const result = await client.query(
+            "select path from photo_scanner.photos"
+        );
+        return result.rows.map((row) => row["path"]);
     } finally {
         client.release();
     }
@@ -125,15 +140,28 @@ async function loadIndexedFilenames() {
 async function main(pool, rootUrl) {
     const filesObject = await gatherAllFilenamesFromFS(rootUrl);
     const readableStream = new FileNamesStream(filesObject.fileNames);
-    const processedPhotos = pipeline(readableStream, omitIndexed(await(loadIndexedFilenames())), exifExtractor(rootUrl), (err) => {});
+    const processedPhotos = pipeline(
+        readableStream,
+        omitIndexed(await loadIndexedFilenames()),
+        exifExtractor(rootUrl),
+        (err) => {}
+    );
     var x = 0;
     for await (const processed of processedPhotos) {
-        sp.setMessage(`Processed ${processed.filePath} (${++x}/${filesObject.filtered})`);
+        sp.setMessage(
+            `Processed ${processed.filePath} (${++x}/${filesObject.filtered})`
+        );
         const client = await pool.connect();
         try {
             await client.query(
-                'insert into photo_scanner.photos(photos_id, name, path, features) values ($1, $2, $3, $4)',
-                [uuid.v4(), path.basename(processed.filePath), processed.filePath, processed.meta]);
+                "insert into photo_scanner.photos(photos_id, name, path, features) values ($1, $2, $3, $4)",
+                [
+                    uuid.v4(),
+                    path.basename(processed.filePath),
+                    processed.filePath,
+                    processed.meta
+                ]
+            );
         } finally {
             client.release();
         }
@@ -141,7 +169,7 @@ async function main(pool, rootUrl) {
 }
 
 const pool = new Pool();
-pool.on('error', (err, client) => {
+pool.on("error", (err, client) => {
     throw new Error(`Unexpected error on idle client, ${err}`);
 });
 
@@ -154,11 +182,11 @@ const rootUrl = args[0];
 const sp = spinner(`Recursively scanning ${rootUrl}`);
 
 main(pool, rootUrl)
-    .catch(err => {
+    .catch((err) => {
         sp.cleanup();
-        console.error('err', err);
-    }) 
-    .finally(stats => {
+        console.error("err", err);
+    })
+    .finally((stats) => {
         sp.stop();
         pool.end();
     });
